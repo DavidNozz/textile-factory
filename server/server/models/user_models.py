c9416e622949f8dd745c750a8034f6b9db7e4cc6
David Nozdrachov <daleno55555@gmail.com>
2026-03-25 11:53:55 +0200

    Auth Server

---
+++ server/server/models/user_models.py
@@ -0,0 +1,42 @@
+from enum import Enum
+from pydantic import BaseModel
+from datetime import datetime
+from typing import Optional
+
+
+class Gender(str, Enum):
+    MALE = "Male"
+    FEMALE = "Female"
+
+
+class Role(str, Enum):
+    EMPLOYEE = "Employee"
+    MANAGER = "Manager"
+    CEO = "CEO"
+
+
+class UserModel(BaseModel):
+    id: Optional[str] = None
+    personal_id: str
+    name: str
+    username: str
+    password: str
+    gender: Gender
+    dob: datetime = datetime.min
+    role: Role = "Employee"
+    manager_id: Optional[str] = None
+    is_blocked: bool = False
+
+
+class UserCredentialsModel(BaseModel):
+    username: str
+    password: str
+
+
+class UpdateUserModel(BaseModel):
+    personal_id: Optional[str] = None
+    name: Optional[str] = None
+    username: Optional[str] = None
+    password: Optional[str] = None
+    gender: Optional[Gender] = None
+    dob: Optional[datetime] = None
---
+++ server/server/routers/user_router.py
@@ -0,0 +1,37 @@
+from uuid import uuid4
+from fastapi import APIRouter, HTTPException
+from http import HTTPStatus
+from server.config.database import users_db
+from server.models.user_models import UserModel, UserCredentialsModel
+
+user_router = APIRouter(prefix="/users")
+
+
+@user_router.post("/register", response_model=UserModel, status_code=HTTPStatus.CREATED)
+async def register_user(user: UserModel):
+    new_user_dict = user.model_dump()
+
+    for existing_user in users_db.values():
+        if existing_user["username"] == new_user_dict["username"]:
+            raise HTTPException(status_code=HTTPStatus.BAD_REQUEST, detail="Username already exists")
+        if existing_user["personal_id"] == new_user_dict["personal_id"]:
+            raise HTTPException(status_code=HTTPStatus.BAD_REQUEST, detail="Personal ID already exists")
+
+    new_user_id = str(uuid4())
+    new_user_dict["id"] = new_user_id
+
+    users_db[new_user_id] = new_user_dict
+    return new_user_dict
+
+
+@user_router.post("/login", response_model=UserModel)
+async def login_user(user_credentials: UserCredentialsModel):
+    user_credentials_dict = user_credentials.model_dump()
+
+    for existing_user in users_db.values():
+        if existing_user["username"] == user_credentials_dict["username"] and existing_user["password"] == user_credentials_dict["password"]:
+            if existing_user["is_blocked"]:
+                raise HTTPException(status_code=HTTPStatus.FORBIDDEN, detail="User is blocked")
+            return existing_user
+
+    raise HTTPException(status_code=HTTPStatus.UNAUTHORIZED, detail="Invalid username or password")
